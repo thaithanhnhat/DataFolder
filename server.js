498b25e33bc212fba84d6513a7b32c302345fc85
Thái Thanh Nhật <97711427+thaithanhnhat@users.noreply.github.com>
2024-09-30 19:12:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const express = require('express');
+const cors = require('cors'); 
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+const path = require('path');
+app.get('/getGame', (req, res) => {
+   
+});
+
+app.post('/addGame', (req, res) => {
+  
+});
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
 
